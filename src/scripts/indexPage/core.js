d6e021a405d32d5cc559ed0f1e937f9f7ac910fc
Dan Le <tle57@gmu.edu>
2024-03-11 17:32:56 -0400

    added onclick for timeslot

--- src/scripts/indexPage/core.js
+++ src/scripts/indexPage/core.js
@@ -1,12 +1,13 @@
 class Day {
-	// date Date object, array of Timeslots
-	constructor(date, timeslots) {
+	// date Date object, array of times
+	constructor(date, times) {
 		this.date = date;
 		this.dayName = date
 			.toLocaleDateString("en-US", { weekday: "short" })
 			.toUpperCase();
 		this.dayMonthString = date.getDate() + "/" + (date.getMonth() + 1);
-		this.timeslots = timeslots;
+		this.times = times;
+		this.timeslots = []; // holds Timeslot objects
 	}
 
 	render() {
@@ -38,8 +39,10 @@ class Day {
 		timeslotContainer.className = "timeslot-container";
 		elem.appendChild(timeslotContainer);
 		// Append timeslots to day
-		this.timeslots.forEach((timeslot) => {
-			timeslotContainer.appendChild(timeslot.render());
+
+		this.times.forEach((time, i) => {
+			this.timeslots.push(new Timeslot(time));
+			timeslotContainer.appendChild(this.timeslots[i].render());
 		});
 
 		return elem;
@@ -50,61 +53,62 @@ class Timeslot {
 	// time is of type string
 	constructor(time) {
 		this.time = time;
+		this.taken = false;
 	}
 
 	render() {
 		const elem = document.createElement("div");
 		elem.className = "timeslot";
 		elem.textContent = this.time;
+		this.element = elem;
+
+		elem.addEventListener("click", () => {
+			this.taken = true;
+			elem.classList.add("taken");
+		});
+
 		return elem;
 	}
 }
 
 // Renders all of the displayed days from current date
-function renderDaysShown(currentDate, timeslots) {
+function renderDaysShown(currentDate, times) {
 	let dayElements = [];
 	let containerElement = document.querySelector(".day-container");
 	for (let i = 0; i <= currentDate.getDay(); i++) {
 		let date = new Date(currentDate);
-		if (currentDate.getDate() === 1) {
-			console.log(currentDate.getDate() - (currentDate.getDay() - i));
-		}
 		date.setDate(currentDate.getDate() - (currentDate.getDay() - i));
-		dayElements.push(new Day(date, timeslots));
+		dayElements.push(new Day(date, times));
 		containerElement.appendChild(dayElements[i].render());
 	}
 
 	for (let i = currentDate.getDay() + 1; i <= 6; i++) {
 		let date = new Date(currentDate);
 		date.setDate(currentDate.getDate() + (i - currentDate.getDay()));
-		dayElements.push(new Day(date, timeslots));
+		dayElements.push(new Day(date, times));
 		containerElement.appendChild(dayElements[i].render());
 	}
 
 	return dayElements;
 }
 
-function updateDaysShown(weekPointer, timeslots) {
+// Updates the shown weeks given the weekPointer
+function updateDaysShown(weekPointer, times) {
 	let dayContainer = document.querySelector(".day-container");
 	dayContainer.innerHTML = "";
 	let newDate = new Date();
 	newDate.setDate(newDate.getDate() + 7 * weekPointer);
-	return renderDaysShown(newDate, timeslots);
+	return renderDaysShown(newDate, times);
 }
 
 // Add times
 document.addEventListener("DOMContentLoaded", () => {
-	const days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
 	const times = ["10:00", "11:00", "12:00", "13:00", "14:00", "15:00"];
-	let timeslots = [];
-	for (let i = 0; i < 6; i++) {
-		timeslots.push(new Timeslot(times[i]));
-	}
 
 	let currDate = new Date();
 
 	// Holds all day divs
-	let daysElementsShown = renderDaysShown(currDate, timeslots);
+	let daysElementsShown = renderDaysShown(currDate, times);
 	// Pointer to indicate which week shown (0 is current week, ++ is next weeks, -- is prev weeks)
 	let weekPointer = 0;
 	console.log(daysElementsShown);
@@ -112,9 +116,9 @@ document.addEventListener("DOMContentLoaded", () => {
 	document.querySelectorAll(".back, .forward").forEach((button) => {
 		button.addEventListener("click", () => {
 			if (button.className === "back") {
-				daysElementsShown = updateDaysShown(--weekPointer, timeslots);
+				daysElementsShown = updateDaysShown(--weekPointer, times);
 			} else {
-				daysElementsShown = updateDaysShown(++weekPointer, timeslots);
+				daysElementsShown = updateDaysShown(++weekPointer, times);
 			}
 		});
 	});
